ef39ff92f459e2d40cda4a2a0f9ac12e92dd83bf
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-18 04:31:30 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,8 +2,9 @@ require 'pry'
 
 def my_all?(collection)
   i = 0 
+  block_return_values = []
   while i < collection.length 
-    yield(collection[i]) #when this code is run and reaches the yield line, it is going to send whatever is passed in as the argument to the block. 
+    block_return_values << yield(collection[i]) #when this code is run and reaches the yield line, it is going to send whatever is passed in as the argument to the block. 
     i += 1 
   end
 end
