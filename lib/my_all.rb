59bc12f553cc408363cd2679586f8f3e4e2c5136
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-18 04:27:26 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0 
+  while i < collection.length 
+    yield(collection[i]) #when this code is run and reaches the yield line, it is going to send whatever is passed in as the argument to the block. 
+    i += 1 
+  end
 end
+
+my_all?([1, 2, 3]) {|i| i < 2}
