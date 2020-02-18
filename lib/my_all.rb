fd76eae1280c0bdc9c502f085533069ae8b1f174
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-18 04:32:51 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,6 +7,12 @@ def my_all?(collection)
     block_return_values << yield(collection[i]) #when this code is run and reaches the yield line, it is going to send whatever is passed in as the argument to the block. 
     i += 1 
   end
+  
+  if block_return_values.include?(false)
+    false
+  else
+    true 
+  end
 end
 
 my_all?([1, 2, 3]) {|i| i < 2}
